ffea55b5a5f51685617e51af165c2e73a3633481
pedroasiqueira <peedro.siqueiraa@hotmail.com>
2024-04-16 11:02:24 -0300

    Tokens

---
+++ dia-1/marryme/budget/admin.py
@@ -0,0 +1,16 @@
+# budget/admin.py
+
+from django.contrib import admin
+from .models import Vendor, Budget, Marriage
+
+
+class BudgetInline(admin.StackedInline):
+    model = Budget
+
+
+class MarriageAdmin(admin.ModelAdmin):
+    inlines = [BudgetInline]
+
+
+admin.site.register(Vendor)
+admin.site.register(Marriage, MarriageAdmin)
